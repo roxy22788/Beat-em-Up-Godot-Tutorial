4e9adb11a45cdd81d92d1bcf3ec6a8cad47204dc
roxy22788 <roxy2298gm@gmail.com>
2026-02-05 13:16:24 -0300

    Part 02/20: Player Movement

--- scenes/characters/character.gd
+++ scenes/characters/character.gd
@@ -1,12 +1,46 @@
 extends CharacterBody2D
 
 
-@export var health: int
 @export var damage: int
+@export var health: int
 @export var speed: float
 
+@onready var animation_player = $AnimationPlayer
+@onready var character_sprite = $CharacterSprite
 
-func _process(delta: float) -> void:
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = direction * speed
+enum State {IDLE, WALK}
+var state = State.IDLE
+
+
+func _process(_delta: float) -> void:
+	handle_input()
+	handle_movement()
+	handle_animations()
+	flip_sprites()
 	move_and_slide()
+
+
+func handle_movement() -> void:
+	if velocity.length() == 0:
+		state = State.IDLE
+	else:
+		state = State.WALK
+
+
+func handle_input() -> void:
+	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = direction * speed
+	
+	
+func handle_animations() -> void:
+	if state == State.IDLE:
+		animation_player.play("idle")
+	elif state == State.WALK:
+		animation_player.play("walk")
+
+
+func flip_sprites() -> void:
+	if velocity.x > 0:
+		character_sprite.flip_h = false
+	elif velocity.x < 0:
+		character_sprite.flip_h = true
