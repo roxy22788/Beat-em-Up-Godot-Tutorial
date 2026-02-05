7f395324b799ed8e4eca12c6229b366674c563f4
roxy22788 <roxy2298gm@gmail.com>
2026-02-05 12:43:02 -0300

    Part 01/20: Setting up Godot

---
+++ scenes/characters/character.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+
+@export var health: int
+@export var damage: int
+@export var speed: float
+
+
+func _process(delta: float) -> void:
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = direction * speed
+	move_and_slide()
